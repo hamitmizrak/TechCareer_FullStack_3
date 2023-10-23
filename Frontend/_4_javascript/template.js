bda8b94d60f1673efbb374a88fe96d4a402cddce
HamitMizrak <hamitmizrak@gmail.com>
2023-10-23 10:19:52 +0300

    Js

---
+++ Frontend/_4_javascript/template.js
@@ -0,0 +1,60 @@
+// single comment
+/* multiple comment */
+
+console.log("console");
+console.warn("console");
+console.error("console");
+window.console.info("console");
+
+// window.alert("deneme44")
+// alert("deneme55")
+
+/////////////////////////////////////////////////////////////////
+var dataValue=14;
+console.log(dataValue);
+
+var dataValue44=44;
+console.log(dataValue44);
+
+var _dataValue44=44;
+console.log(_dataValue44);
+
+var $_dataValue44=44;
+console.log($_dataValue44);
+
+// var `~&444$_dataValue44=44;
+
+
+var data=99;
+console.log( typeof data);
+
+var data="Kelime";
+console.log( typeof data);
+
+var data=3>1;
+console.log( typeof data);
+
+// hoisting
+data=56566;
+var data ;
+console.log(data);
+
+/////////////////////////////////////////////////////////////////
+var globalVariable="String-1"
+console.log(globalVariable);
+
+var globalVariable="String-2"
+console.log(globalVariable);
+
+let localVariable=true;
+ localVariable=false;
+
+ const localVariable1=true;
+
+ /////////////////////////////////////////////////////////////////
+// Operatorler
+ // + - * / % ! &&=VE ||=VEYA
+ // x++ ,  ++x , x-- , --x
+ // x++;
+ // x=x+1;
+ // x+=1;
