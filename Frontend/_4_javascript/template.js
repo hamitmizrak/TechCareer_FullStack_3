b6291164a55d74a034657924d121dafa016c118c
HamitMizrak <hamitmizrak@gmail.com>
2023-10-23 12:22:53 +0300

    Loop

--- Frontend/_4_javascript/template.js
+++ Frontend/_4_javascript/template.js
@@ -250,9 +250,57 @@ conditional4();
 
 //////////////////////////////////////////////////////////////////
 // Loop
+
 // iterative for
+const loop1=()=>{
+    let sum=0;
+    for (let i = 1; i <=9; i++) {
+        document.writeln(i); 
+        sum+=i;
+    }
+    document.writeln("Sonuç: "+sum)
+}
+loop1()
+// for(;;){}
+document.writeln("<br/>------------<br/>"); 
+
 // while
+const loop2=()=>{
+    let sum=0;
+
+    let i = 1;
+    while ( i <=9) {
+        document.writeln(i); 
+        sum+=i;
+        i=i+1;
+    }
+    document.writeln("Sonuç: "+sum)
+}
+loop2()
+// while(true){}
+document.writeln("<br/>------------"); 
+
+
 // do-while
+document.writeln("<br/>------------<br/>"); 
+
+// while
+const loop3=()=>{
+    let sum=0;
+
+    let i = 1;
+
+    do{
+        document.writeln(i); 
+        sum+=i;
+        i=i+1;
+    }
+    while ( i <=9);
+    document.writeln("Sonuç: "+sum)
+}
+loop3()
+// while(true){}
+document.writeln("<br/>------------"); 
 
 //////////////////////////////////////////////////////////////////
 // Dizi
@@ -279,3 +327,109 @@ conditional4();
 // Form input Handling && jQuery
 
 // callback, promise, asyn-await
+
+
+
+//////////////////////////////////////////////////////////////////// 
+// ÖDEV
+// Örnek-1
+// Kullanıcıdan aldığımız 2 sayıyı toplayan JS kodu yazınız ? (prompt)
+// let number1,number,sum=0;
+// number1=Number(prompt("Lütfen 1.sayıyı giriniz"));
+// number2=Number(prompt("Lütfen 2.sayıyı giriniz"));
+// console.log(number1+number2);
+// Örnek-2
+/* 
+    Kullanıcıdan aldığımız sayıyı dereceden fahrenhayt'a çeviren JS kodu yazınız ?
+    Formül: (derece*9/5)+32   
+*/
+// let degree=0,fahranhayt=0;
+// degree=Number(prompt("Lütfen Dereceyi giriniz"));
+// fahranhayt=(degree*9/5)+32;
+// console.log(fahranhayt);
+
+
+// Örnek-3
+// y=3x+4k ==> 1.dereceden 2 bilinmeyenli denklem algoritma JS kodu yazınız ?
+// Kullanıcı tarafından alınan x ve kdeğerlerini hesaplayan algoritma yazınız ?
+// x=2 k=3 y=? (y=18)
+// let x=0,y=0,k=0;
+// x=Number(prompt("Lütfen X giriniz"));
+// k=Number(prompt("Lütfen K giriniz"));
+// y=3*x+4*k;
+// console.log("Y sonucu:"+y);
+
+// örnek-4 operatör işlemleri: aşağıdaki örneği javascript ile yapalım
+// 4+3*2(3:3-1*6+9:1+(3:3))
+// console.log(4+3*2*(3/3-1*6+9/1+(3/3)));
+
+
+//// MATH ÖDEVİ
+// Math Ödev
+// Kullanıcıda tarafından aldığımız 2 tane sayının (prompt)
+// s1-) Bu sayılardan en küçüğü nedir ? (min)
+// s2-) Bu sayılardan en büyüğü nedir ? (max)
+// s3-) Bu sayılardan en büyüğü karekökü nedir ? (sqrt(max))
+// s4-) Bu sayılardan en büyüğü karekökünün üste yuvarlıyalım nedir ? (ceil(sqrt(max)))
+// s5-) Bu sayılardan en küçüğü mutlak değeri nedir ? (abs(min))
+// s6-) Bu sayılardan en küçüğü alt taban en büyüğü üst taban şeklinde üstünü alalım(pow(min,max))
+
+// Örnek: Aşağıdaki örnekleri math ile çözelim ?
+// -5.9 sayıyının aşağıdaki işlemleri yaptıralım ?
+// 1-mutlak değeri alsın 5.9 (Math.abs)
+// 2-yukarı yuvarlama yapsın  6.0 (Math.ceil)
+// 3-karesini alsın 36.00 (Math.pow(sayı,2))
+// 4-çıkan sayının karekökü alsın (Math.sqrt(sayı))
+// 6-)çıkan sonucu 5 bölsün 6/5=1
+// 7-) iki sayı arasından karşılaştırma yapsın en küçüğünü alsın ve 1 ve 5
+// 8-) küçük sayı eğer tekse 3 eklesin çiftse 5 eklesin 
+
+// ÖDEV: 
+// Kullanıcı tarafından girilen bir sayının negatif mi, pozitif mi ? bunu yazan JS code yazınız.
+// (Dikkat: normal  function ile yazınız)
+
+// Kullanıcı tarafından girilen bir sayının tek mi, çift mi olduğunu yazan JS code yazınız.
+// (Dikkat:anonymous function ve ternary ile yazınız)
+
+
+// Şimdiki zamanın saatine göre hesaplama yapan switch-case ile  JS code yazınız.
+// (Dikkat: arrow function ile yazınız) Zaman için new Date().getHours() ile alabilirsiniz
+
+
+//NOT: örneklerimizi function,anonymous ve arrow function ile yapalım.
+//NOT: değişken olarak var,let,const kullanalım.
+//Örnek-1
+//y=3x+4k ==>1.dereceden2bilinmeyenlidenklem algoritması
+//Kullanıcı tarafından alınan x ve kdeğerlerini hesaplayan algoritma yazınız ?
+//örnek-2
+//Kullanıcı tarafından alınan dereceyi  Fahrenhayta çeviren algoritma yapalım.
+//Formül: (derece*9/5)+32
+//örnek-3 operatör işlemleri: aşağıdaki örneği javascript ile yapalım
+//4+3*2(3:3-1*6+9:1+(3:3))
+
+
+
+//Örnek-4: Aşağıdaki örnekleri math ile çözelim ?
+// -5.9 sayıyının aşağıdaki işlemleri yaptıralım ?
+// 1-mutlak değeri alsın 5.9
+// 2-yuvarlama yapsın  6.0
+// 3-karesini alsın 36.00
+// 4-karekök alsın 6.0
+// 5-yuvarlama yapsın 6.0
+// 6-)çıkan sonucu 5 bölsün 6/5=1
+// 7-) iki sayı arasından karşılaştırma yapsın en küçüğünü alsın ve 1 ve 5
+// 8-) küçük sayı eğer tekse 3 eklesin çiftse 5 eklesin 
+// Örnek-5 : kullanıcı tarafından girilen bir sayıyı negatif mi pozitif mi olduğu ekran yazdıran algoritma ?
+//NOT: cast kullalım ?
+// Örnek-6 : kullanıcı tarafından password ve repassword alalım sonrasında bu iki değeri karşılaştırma yapalım
+//eğer aynı girilirse aynı veri yoksa birbirine uymadı yazan algoritma yapalım
+
+
+// ********** ÖNEMLi ******************
+// ÖDEV 
+// kullanıcıdan aldığımız isim soyisim (boşluk var)
+// ilk karakter göstersin sonraki kelimeleri masking (maskeleme yapsın)
+// eğer kullanıcı isim ve soyisimi ilk karakteri küçük girmişse büyük olsun mutlaka
+// Hamit Mızrak
+// H**** MIZ***
+// ipucu: charAt, substring, indexOf, döngü, karar mekanizma
